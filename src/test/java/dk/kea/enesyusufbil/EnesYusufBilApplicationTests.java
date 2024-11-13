5ed07c1b103f2d2a48098770009103e60dc6fdbc
Yudo95 <yudo0001@stud.kea.dk>
2024-11-13 15:45:05 +0100

    Initial commit

---
+++ src/test/java/dk/kea/enesyusufbil/EnesYusufBilApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.kea.enesyusufbil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnesYusufBilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
